facea266345887e975046aca6791777793f6d06c
Mudita Jain <mudita21jain@gmail.com>
2025-06-07 13:15:40 +0530

    `Hero Layout`

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const words = [
+    {text: "Ideas", imgPath: "/images/ideas.svg"},
+    {text: "Designs", imgPath: "/images/designs.svg"},
+    {text: "Concepts", imgPath: "/images/concepts.svg"},
+    {text: "Code", imgPath: "/images/code.svg"},
+    {text: "Ideas", imgPath: "/images/ideas.svg"},
+    {text: "Designs", imgPath: "/images/designs.svg"},
+    {text: "Concepts", imgPath: "/images/concepts.svg"},
+    {text: "Code", imgPath: "/images/code.svg"},
+    {text: "Ideas", imgPath: "/images/ideas.svg"},
+    {text: "Designs", imgPath: "/images/designs.svg"},
+    {text: "Concepts", imgPath: "/images/concepts.svg"},
+    {text: "Code", imgPath: "/images/code.svg"},
+]
